9f0eb863118e3c34d4c3e92115afb7c9f20dbacb
Connor Cahill <connorcahill913@gmail.com>
2018-10-31 21:42:04 -0700

    Setup Industry Model

---
+++ models/industries.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const IndustrySchema = new Schema ({
+    name: { type: String, required: true }
+    //users: { type: Schema.Types.ObjectId, ref: 'User' },
+    //feed: { type: Schema.Types.ObjectId, ref: 'Feed'},
+    //threads: { type: Schema.Types.ObjectId, ref: 'Thread'},
+
+})
+
+
+module.exports = mongoose.model('Industry', IndustrySchema);
