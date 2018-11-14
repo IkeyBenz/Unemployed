f63b8ec856e469cce3bda716f2384a5cd230ba13
Ikey Benzaken <ikey.benz@gmail.com>
2018-11-14 12:11:44 -0800

    Added users template

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.static('public'));
 app.use('/', authController);
 app.use('/', postController);
 app.use('/', commentController);
-app.use('/', require('./controllers/users'));
+app.use('/users', require('./controllers/users'));
 
 app.listen(5000, console.log("Listening on 5000"));
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,20 @@
 const users = require('express').Router();
 const User = require('../models/user');
 
-users.get('/users/:id', (req, res) => {
+// Top level forwarding from /users route
+
+
+users.get('/', (req, res) => {
+    User.find({}).then(users => {
+        res.render('users', { users: users });
+    });
+});
+
+users.get('/:id', (req, res) => {
     User.findById(req.params.id).then(user => {
         // For some reason 'user._id == req.user._id' was return false
         // So this was my quick fix...
-        if (String(user._id) == String(req.user._id)) {
+        if (req.user && String(user._id) == String(req.user._id)) {
             // Visiting his own profile
             res.render('profile', { user: req.user, profile: user, authorizedEditor: true });
         } else {
@@ -15,7 +24,7 @@ users.get('/users/:id', (req, res) => {
     });
 });
 
-users.patch('/users/:id/update', (req, res) => {
+users.patch('/:id/update', (req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body, () => {
         User.findById(req.params.id).then(user => {
             res.render('profile', { user: req.user, profile: user, authorizedEditor: true });
