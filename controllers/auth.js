1aecb87be3c10a8fccf1c57645826ba537f33443
IkeyBenz <ikey.benz@gmail.com>
2018-11-20 17:42:08 -0500

    Cleaned up navbar, added logic for which signin/signout/signup buttons display in nav

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email']}))
 
-router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login'}),
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
 function(req, res) {
     res.redirect('/');
 });
