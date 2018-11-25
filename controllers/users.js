ce996df19d242e196626f12d5fd5716402f466b6
Connor Cahill <connorcahill913@gmail.com>
2018-11-24 17:04:05 -0800

    made small formatting changes

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 
 users.get('/users/:id', (req, res) => {
     User.findById(req.params.id).then(user => {
-        // For some reason 'user._id == req.user._id' was return false
+        // For some reason 'user._id == req.user._id' was returning false
         // So this was my quick fix...
         if (String(user._id) == String(req.user._id)) {
             // Visiting his own profile
