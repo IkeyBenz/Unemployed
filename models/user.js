672fe6f797f86320acad00553dee8e2a455a2604
Ikey Benzaken <ikey.benz@gmail.com>
2018-12-05 10:21:01 -0800

    more updates to the user model

--- models/user.js
+++ models/user.js
@@ -5,12 +5,13 @@ const UserSchema = new Schema({
     createdAt: { type: Date, default: Date.now() },
     updatedAt: { type: Date },
     name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, select: false },
     bio: { type: String, required: false },
     profilePicture: { type: String, required: false },
     age: { type: Number, required: false },
     posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
     comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-    email: { type: String, required: true },
     type: { type: String, required: true, default: 'user' }
 });
 
