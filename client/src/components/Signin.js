4c6613f19764414d9079c3e9cb1250966759c004
Connor Cahill <connorcahill913@gmail.com>
2018-12-05 11:20:03 -0800

    added signin and signup form and some styling to the view.

---
+++ client/src/components/Signin.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router';
+import './Signin.css';
+
+
+class Signin extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            formSubmitted: false
+        }
+
+        this.signinUser = this.signinUser.bind(this);
+    }
+    
+    signinUser(e) {
+        const form = e.target;
+        const data = new FormData(form);
+        const email = data.get('email');
+        const password = data.get('password');
+        const user = {
+            email: email,
+            password: password
+        }
+        this.setState({
+            formSubmitted: true
+        })
+
+        
+
+        axios({
+            method: 'post',
+            url: '/signin',
+            data: user
+        })
+        .then(res => console.log('user is logged in'))
+        .catch(err => console.log(err));
+    }
+
+    render() {
+        if ( this.state.formSubmitted === true ) {
+            return <Redirect push to="/" />
+        }
+        return (
+            <div className="transparent-overlay-background">
+                <div className="Signin-container">
+                    <h2 className="auth-form-header">Get Signed In</h2>
+                    <form onSubmit={ this.signinUser } className="signin-form">
+                        <label for="email" className="signin-form-labels">Email:</label>
+                        <input className="auth-input" type="text" name="email" placeholder="Enter Your Email" />
+                        <label for="password" className="signin-form-labels" >Password:</label>
+                        <input className="auth-input" name="password" type="password" placeholder="Enter Your Password" />
+                        <div className="auth-btns-container">
+                            <button className="auth-btn">Sign In</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+export default Signin;
